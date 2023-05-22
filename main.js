85f1cc57a444ccf4c6f0bf4badf2a7cf4fce6d18
BDu4rTe <brianschierduarte@outlook.com>
2023-05-22 17:53:33 -0300

    ajustes no layout para telas menores

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".menu-nav");
+
+menu.onclick = () => {
+    if (menu.name == "menu-outline"){
+        menu.name  = "close-outline";
+    } else{
+        menu.name  = "menu-outline";
+    } 
+    nav.classList.toggle("open");
+}
+
 function setGaugeFill(param, fill, complemento=null){
     let realfill = fill * (360 / 100);
     document.getElementById(param).style.background = param == "umidade" ? `conic-gradient(#95C4E8 ${realfill}deg, #4C515C 0deg)` : `conic-gradient(#E87D4F ${realfill}deg, #4C515C 0deg)`  ;
@@ -6,5 +18,8 @@ function setGaugeFill(param, fill, complemento=null){
 
 setGaugeFill("temperatura", 24, "°");
 setGaugeFill("umidade", 68, "%");
-// fazer comunicacao com o protocolo mqtt
+
+
+
+
 
